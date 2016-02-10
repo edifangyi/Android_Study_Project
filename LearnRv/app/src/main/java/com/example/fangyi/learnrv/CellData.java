2f7fc31bf15537b6f0c7ed11bc6b5cdbe440571c
FANG <fangyi1896@gmail.com>
2016-02-10 21:18:50 +0800

    基本布局

---
+++ LearnRv/app/src/main/java/com/example/fangyi/learnrv/CellData.java
@@ -0,0 +1,15 @@
+package com.example.fangyi.learnrv;
+
+/**
+ * Created by FANGYI on 2016/2/10.
+ */
+public class CellData {
+
+    public CellData(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String title = "title";
+    public String content = "content";
+}
