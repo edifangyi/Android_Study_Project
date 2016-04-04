3be03d6db648253daa3d78929bdd47d1cd65d70c
FANG <fangyi1896@gmail.com>
2016-04-04 22:04:46 +0800

    JSON

--- Android JSON 解析库的使用.java
+++ Android JSON 解析库的使用.java
@@ -237,6 +237,8 @@ public class Tag {
 }
 
 
+/**
+ 
 
 
 
@@ -244,35 +246,101 @@ public class Tag {
 
 
 
+ */
 
 
 
+Fast-json 的基本用法
 
+    ·定义实体类
+    ·根据需要可以讲 JSON 生成单个实体或列表实体集合
 
+Book 和 Tag 类 跟 Gson一样
 
+/**
+ 
+ */
 
+package com.example.fangyi.jsondemo.Fastjson;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.fangyi.jsondemo.Fastjson.bean.Book;
+import com.example.fangyi.jsondemo.R;
 
+import java.util.ArrayList;
+import java.util.List;
 
+public class Fastjson extends AppCompatActivity {
 
+    private String url = "https://api.douban.com/v2/book/1220562";
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fastjson);
 
+        getData();
+    }
 
+    public void getData() {
+        StringRequest request = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                dealData(s);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
 
+            }
+        });
+    }
 
+    public void dealData(String result) {
+        Book book = JSON.parseObject(result, Book.class);//解析一条数据
 
+        //多个book列表，如果result是多本书的话，可以解析成列表，数组集合
+        List<Book> books = JSON.parseObject(result, new TypeReference<List<Book>>(){});
 
+        //想把很多个对象，转换成JSON格式的话
+        Book book1 = new Book();
+        book1.setTitle("标题1");
+        Book book2 = new Book();
+        book2.setTitle("标题2");
+        Book book3 = new Book();
+        book3.setTitle("标题3");
 
+        JSON.toJSON(book1);//一个对象对象转换成JSON格式的话
 
+        //集合转换JSON格式
+        List<Book> list = new ArrayList<Book>();
+        list.add(book1);
+        list.add(book2);
+        list.add(book3);
+        JSON.toJSON(list);
 
 
 
 
+        book.getTitle();
+        book.getPublisher();
+        book.getSummary();
+        book.getTags().size();
 
+    }
+}
 
 
 
+/**
+ 
 
 
 
@@ -280,41 +348,227 @@ public class Tag {
 
 
 
+ */
 
+实现效果:
 
+1.分别使用 Gson 和 Fast-json 实现复杂的 Json 数据解析
+2.实现模拟图书列表 Json 数据的解析展现
 
+/**
+ 
+ */
 
 
 
+package com.example.fangyi.jsondemo.BookBusiness;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
+import com.alibaba.fastjson.JSON;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.fangyi.jsondemo.BookBusiness.adapter.BookListAdapter;
+import com.example.fangyi.jsondemo.BookBusiness.bean.Book;
+import com.example.fangyi.jsondemo.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
 
+public class BookBusiness extends AppCompatActivity {
 
+    private String url = "http://apis.juhe.cn/goodbook/catalog?key=cffd90e2deb9501950b3b5746e55236a&dtype=json";
+    private ListView lv;
+    private BookListAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_business);
 
+        lv = (ListView) this.findViewById(R.id.lv);
 
+        getData();
+    }
 
+    private void getData() {
+        StringRequest request = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                try {
+                    dealData(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
 
+            }
+        });
+        Volley.newRequestQueue(getApplicationContext()).add(request);
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    private void dealData(String result) throws JSONException {
+        JSONObject object = new JSONObject(result);
+        ArrayList<Book> books = (ArrayList<Book>) JSON.parseArray(object.getString("result"), Book.class);
+
+//        Gson gson = new Gson();
+//        Type listType = new TypeToken<ArrayList<Book>>(){}.getType();
+//
+//        JSONObject object = new JSONObject(result);
+//
+//        ArrayList<Book> books = gson.fromJson(object.getString("result"), listType);
+
+
+        adapter = new BookListAdapter(this, books);
+        lv.setAdapter(adapter);
+    }
+}
+
+
+/**
+ 
+ */
+
+
+package com.example.fangyi.jsondemo.BookBusiness.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.fangyi.jsondemo.BookBusiness.bean.Book;
+import com.example.fangyi.jsondemo.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by FANGYI on 2016/4/4.
+ */
+public class BookListAdapter extends BaseAdapter {
+
+    private Context con;
+    private ArrayList<Book> list;
+
+
+    public BookListAdapter(Context context, ArrayList<Book> books) {
+        this.con = context;
+        this.list = books;
+    }
+
+    /**
+     * How many items are in the data set represented by this Adapter.
+     *
+     * @return Count of items.
+     */
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    /**
+     * Get the data item associated with the specified position in the data set.
+     *
+     * @param position Position of the item whose data we want within the adapter's
+     *                 data set.
+     * @return The data at the specified position.
+     */
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    /**
+     * Get the row id associated with the specified position in the list.
+     *
+     * @param position The position of the item within the adapter's data set whose row id we want.
+     * @return The id of the item at the specified position.
+     */
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    /**
+     * Get a View that displays the data at the specified position in the data set. You can either
+     * create a View manually or inflate it from an XML layout file. When the View is inflated, the
+     * parent View (GridView, ListView...) will apply default layout parameters unless you use
+     * {@link LayoutInflater#inflate(int, ViewGroup, boolean)}
+     * to specify a root view and to prevent attachment to the root.
+     *
+     * @param position    The position of the item within the adapter's data set of the item whose view
+     *                    we want.
+     * @param convertView The old view to reuse, if possible. Note: You should check that this view
+     *                    is non-null and of an appropriate type before using. If it is not possible to convert
+     *                    this view to display the correct data, this method can create a new view.
+     *                    Heterogeneous lists can specify their number of view types, so that this View is
+     *                    always of the right type (see {@link #getViewTypeCount()} and
+     *                    {@link #getItemViewType(int)}).
+     * @param parent      The parent that this view will eventually be attached to
+     * @return A View corresponding to the data at the specified position.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if (holder == null) {
+            convertView = View.inflate(con, R.layout.book_business_item_list, null);
+            holder = new ViewHolder();
+            holder.tv = (TextView) convertView.findViewById(R.id.tv);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        Book book = list.get(position);
+        holder.tv.setText(book.getCatalog() + "\n" + book.getId());
+        return convertView;
+    }
+
+    class ViewHolder {
+        TextView tv;
+    }
+}
+
+
+
+/**
+ 
+ */
+
+
+package com.example.fangyi.jsondemo.BookBusiness.bean;
+
+/**
+ * Created by FANGYI on 2016/4/4.
+ */
+public class Book {
+    private String id;
+    private String catalog;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCatalog() {
+        return catalog;
+    }
+
+    public void setCatalog(String catlog) {
+        this.catalog = catlog;
+    }
+}
 
