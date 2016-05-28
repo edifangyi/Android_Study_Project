27529c1f0b3f001c661bfb62b7941dc80f62d020
FANGYI <fangyi1896@gmail.com>
2016-05-28 22:21:37 +0800

    2016年5月28日 22:21:21
    更新了一下UI上的细节和 九宫格

--- 手机安全卫士/MobileSafe/app/src/main/java/com/fangyi/mobilesafe/Guide/WelcomeActivity.java
+++ 手机安全卫士/MobileSafe/app/src/main/java/com/fangyi/mobilesafe/Guide/WelcomeActivity.java
@@ -1,14 +1,21 @@
 package com.fangyi.mobilesafe.Guide;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.os.SystemClock;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,9 +23,13 @@ import com.fangyi.mobilesafe.activity.MainActivity;
 import com.fangyi.mobilesafe.R;
 import com.fangyi.mobilesafe.utils.StreamTools;
 
+import net.tsz.afinal.FinalHttp;
+import net.tsz.afinal.http.AjaxCallBack;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -64,6 +75,7 @@ public class WelcomeActivity extends Activity {
 
                 case SHOW_UPDATE_DIALOG://弹出升级对话框
                     Log.e(TAG, "有新版本，弹出升级对话框");
+                    showUpdateDialog();
                     break;
 
                 case URL_ERROR://URL错位
@@ -92,17 +104,30 @@ public class WelcomeActivity extends Activity {
         setContentView(R.layout.welcome);
         //引导页
         init();
+        alphaAnimation();
     }
 
     private TextView tvWelcomeVersion;
+    private TextView tvWelcomeVpdateinfo;
 
     private void assignViews() {
         tvWelcomeVersion = (TextView) findViewById(R.id.tv_welcome_version);
+        tvWelcomeVpdateinfo = (TextView) findViewById(R.id.tv_welcome_updateinfo);
         //设置版本号
         tvWelcomeVersion.setText("版本名：" + getVersionName());
         //检测是否有新版本
         checkVersion();
     }
+
+    /**
+     * WelcomeActivity动画效果
+     */
+    public void alphaAnimation() {
+        AlphaAnimation aa = new AlphaAnimation(0.2f, 1.0f);
+        aa.setDuration(1000);
+        findViewById(R.id.rl_welcome_root).startAnimation(aa);
+    }
+
 
     /**
      * 得到版本名称
@@ -131,6 +156,7 @@ public class WelcomeActivity extends Activity {
             public void run() {
                 //请求网络，得到最新的版本信息
                 Message msg = Message.obtain();
+                long startTime = System.currentTimeMillis();
                 try {
                     URL url = new URL(getString(R.string.serverurl));
                     HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -176,6 +202,11 @@ public class WelcomeActivity extends Activity {
                     e.printStackTrace();
                     msg.what = JSON_ERROR;
                 } finally {
+                    long endTime = System.currentTimeMillis();
+                    long dTime = endTime - startTime;//花的时间没有到2秒
+                    if (dTime < 2000) {
+                        SystemClock.sleep(2000-dTime);
+                    }
                     handler.sendMessage(msg);
                 }
             }
@@ -211,6 +242,79 @@ public class WelcomeActivity extends Activity {
         Intent i = new Intent(WelcomeActivity.this,Guide.class);
         startActivity(i);
         finish();
+    }
+
+    /**
+     * 弹出对话框
+     */
+    private void showUpdateDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("提示");
+
+//        //强制升级使用
+//        builder.setCancelable(false);
+
+        //监听事件
+        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                dialog.dismiss();
+                goHome();
+            }
+        });
+
+        builder.setMessage(description);
+        builder.setNegativeButton("下次再说", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                goHome();
+            }
+        });
+        builder.setPositiveButton("立刻升级", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+                    //1.下载APK
+                    FinalHttp http = new FinalHttp();
+                    http.download(apkurl, Environment.getExternalStorageDirectory() + "/mobilesafe2.0apk", new AjaxCallBack<File>() {
+                        @Override
+                        public void onSuccess(File file) {
+                            super.onSuccess(file);
+                            installApk(file);
+                            Toast.makeText(WelcomeActivity.this, "下载成功了", Toast.LENGTH_SHORT).show();
+                        }
+
+                        private void installApk(File file) {
+                            Intent intent = new Intent();
+                            intent.setAction("android.intent.action.VIEW");
+                            intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+                            startActivity(intent);
+                        }
+
+                        @Override
+                        public void onFailure(Throwable t, int errorNo, String strMsg) {
+                            t.printStackTrace();//打印错误日志
+                            Toast.makeText(WelcomeActivity.this, "下载失败了", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onLoading(long count, long current) {
+                            super.onLoading(count, current);
+                            tvWelcomeVpdateinfo.setVisibility(View.VISIBLE);
+                            int progess = (int) (current*100/count);
+                            tvWelcomeVpdateinfo.setText("下载进度：" + progess + "%");
+                        }
+                    });
+                    //2.替换安装
+                } else {
+                    Toast.makeText(WelcomeActivity.this, "sdcard", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        builder.show();
     }
 
 }
--- 手机安全卫士/MobileSafe/app/src/main/java/com/fangyi/mobilesafe/activity/MainActivity.java
+++ 手机安全卫士/MobileSafe/app/src/main/java/com/fangyi/mobilesafe/activity/MainActivity.java
@@ -1,7 +1,13 @@
 package com.fangyi.mobilesafe.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.fangyi.mobilesafe.R;
 
@@ -11,5 +17,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        assignViews();
     }
+
+    private GridView listMain;
+    private static final  String names[] = {"手机防盗", "通信卫士", "应用管理", "进程管理", "流量统计", "手机杀毒", "缓存管理", "高级功能", "设置中心"};
+    private static final  int ids[] = {R.drawable.plane, R.drawable.pointer, R.drawable.rss, R.drawable.star, R.drawable.ticket, R.drawable.twitter, R.drawable.user, R.drawable.view, R.drawable.weather};
+    private void assignViews() {
+        listMain = (GridView) findViewById(R.id.list_main);
+        //设置适配器
+
+        listMain.setAdapter(new HomeAdapter());
+    }
+
+    private class HomeAdapter extends BaseAdapter {
+
+        //返回长度
+        @Override
+        public int getCount() {
+            return names.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = View.inflate(MainActivity.this, R.layout.main_item, null);
+            ImageView ivIcon = (ImageView) view.findViewById(R.id.iv_icon);
+            TextView tvName = (TextView) view.findViewById(R.id.tv_name);
+            ivIcon.setImageResource(ids[position]);
+            tvName.setText(names[position]);
+            return view;
+        }
+    }
+
 }
